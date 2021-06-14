cd5c9ff1ab698e57ec4bdbc1897bbf1b9db63fba
MatthewSams <56139555+MatthewSams@users.noreply.github.com>
2021-06-14 17:40:48 -0400

    Revert "Revert "package-lock.json""
    This reverts commit 945c97416238ce5e5f51720a1cf2140de948b571.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+        <h1 class="intro_text default_text_style">Share events in Seconds</h1>
+          <button class="create_event_button default_text_style">Create New Event</button>
+        <h2 class="desc_header_text default_text_style">How it Works</h2>
+          <p class="desc_text default_text_style">
+          To create an event, click on Create Event then 
+          fill out basic information like Event Name, Location...
+          </p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
