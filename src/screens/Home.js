b5bee5f83cafb9e34b45570eda9534326826d2b4
Kenta <kenta.yaegashi@gmail.com>
2022-01-24 20:18:04 -0800

    footer and changed spacing of homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,3 +1,4 @@
+
 import React from "react";
 import {
     Image,
@@ -23,8 +24,8 @@ export const HomeScreen = ({ navigation }) => {
                 <View style={[GlobalStyles.cardSection]}>
                     <View style={[styles.titleSection]}>
                         {/*Header*/}
-                        <Text style={[GlobalStyles.headerText, {marginTop: "0.04rem", marginBottom: "5.8rem", marginHorizontal: "1.5625rem", fontSize: "2.052rem", lineHeight: "3.125rem"}]}>
-                            The world’s fastest{"\n"}
+                        <Text style={[GlobalStyles.headerText, {marginTop: "0.04rem", marginBottom: "0.8rem", marginHorizontal: "1.5625rem", fontSize: "2.052rem", lineHeight: "3.125rem"}]}>
+                            The World’s Fastest{"\n"}
                             {/*Inline Bold Text*/}
                             <Text style={GlobalStyles.extraBold}>Event Organizer</Text>
                         </Text>
@@ -34,7 +35,7 @@ export const HomeScreen = ({ navigation }) => {
                             source={require("../assets/HomeIcon-1.png")}
                         />
                         {/*Create Event Button*/}
-                        <TouchableOpacity style={[GlobalStyles.submitButton, {marginTop: "5.776rem", marginBottom: "4.6875rem"}]}
+                        <TouchableOpacity style={[GlobalStyles.submitButton, {marginTop: "4.776rem", marginBottom: "4.6875rem"}]}
                             onPress={() => {
                                 navigation.navigate("Create");
                             }}
@@ -117,7 +118,7 @@ export const HomeScreen = ({ navigation }) => {
                         </Text>
 
                         <Text style={[GlobalStyles.bodyText, {marginBottom: "1.875rem", color: "white", marginHorizontal: "2.906rem"}]}>
-                        Details get burried easily in the group chat. Instead of re-typing your event details each time, send a link
+                            Details get burried easily in the group chat. Instead of re-typing your event details each time, send a link
                         </Text>
                       
                         <View style={[GlobalStyles.columnContainer, {marginBottom: "2.25rem"}]}>
@@ -157,69 +158,64 @@ export const HomeScreen = ({ navigation }) => {
                             style={{width:"20.3125rem", height:"25.0625rem", margin: "0 auto 0 auto"}}
                             source={require("../assets/text-convo.png")}
                         >
-                            
                         </Image>
                     </View>
                 </View>
 
 
-    {/*Fifth Card/Section: "Right into their calendar" */}
-    <View style={[GlobalStyles.cardSection, {backgroundColor: GlobalColors.white}]}>
-        <View style={[styles.titleSection]}>
-            {/*Header*/}
-            <Text style={[GlobalStyles.headerText, {marginBottom: "1.875rem"}]}>
-                    Right into their Calendar
-            </Text>
-                       
-            {/*  Adding multiple calendar image*/}
-            <Image
-                style={{width:"9.1875rem", height:"6.6875rem", margin: "0 auto 0 auto", marginBottom: "1.875rem"}}
-                source={require("../assets/calendar-multiple.png")}
-            >  
-            </Image>
+     {/*Fifth Card/Section: "Right into their calendar" */}
+                <View style={[GlobalStyles.cardSection, {backgroundColor: GlobalColors.white}]}>
+                    <View style={[styles.titleSection]}>
+                        
+                        {/*Header*/}
+                        <Text style={[GlobalStyles.headerText, {marginBottom: "1.875rem"}]}>
+                            Right into their Calendar
+                        </Text>
+                                
+                        {/*  Adding multiple calendar image*/}
+                        <Image
+                            style={{width:"9.1875rem", height:"6.6875rem", margin: "0 auto 0 auto", marginBottom: "1.875rem"}}
+                            source={require("../assets/calendar-multiple.png")}
+                        > 
+                        </Image>
 
+                        {/*Body text*/}
+                        <Text style={[GlobalStyles.bodyText, {marginBottom: "4.75rem", color: "#6F7480", marginHorizontal: "2.906rem"}]}>
+                            UGoing lets you add events directly into your preferred calendar.
+                            {'\n'}
+                            {'\n'}
+                            No more digging through messages or forgotten reminders - Everything you need to know front and center where you expect it
+                        </Text>
+                    </View>
+                </View>
 
-            {/*Body text*/}
-            <Text style={[GlobalStyles.bodyText, {marginBottom: "4.75rem", color: "#6F7480", marginHorizontal: "2.906rem"}]}>
-            UGoing lets you add events directly into your preferred calendar.
-            {'\n'}
-            {'\n'}
-            No more digging through messages or forgotten reminders - Everything you need to know front and center where you expect it
-            </Text>
-        </View>
-        </View>
+    {/*sixth: Tile Section: and it's all free */}
+                <View style={[GlobalStyles.tileSection, styles.titleSection, {backgroundColor: GlobalColors.white}]}>
+                    <Text style={[GlobalStyles.headerText, {marginBottom: "3rem"}]}>
+                        And it’s all free!
+                    </Text>
 
+                    <Text style={[GlobalStyles.bodyText, {marginBottom: "4.75rem", color: GlobalColors.lightBlack, marginHorizontal: "2.906rem", textAlign: "center", marginBottom: "4.5625rem"}]}>
+                        Create as many events as you want
+                        {'\n'}
+                        Free to create events
+                        {'\n'}
+                        Free for your guests
+                    </Text>
 
-  {/*sixth: Tile Section */}
-
-  <View style={[GlobalStyles.tileSection, styles.titleSection, {backgroundColor: GlobalColors.white}]}>
-            <Text style={[GlobalStyles.headerText, {marginBottom: "3rem"}]}>
-            And it’s all free!
-            </Text>
-
-            <Text style={[GlobalStyles.bodyText, {marginBottom: "4.75rem", color: GlobalColors.lightBlack, marginHorizontal: "2.906rem", textAlign: "center", marginBottom: "4.5625rem"}]}>
-            Create as many events as you want
-            {'\n'}
-            Free to create events
-            {'\n'}
-            Free for your guests
-            </Text>
-
-            {/*Create Event Button*/}
-            <TouchableOpacity style={[GlobalStyles.submitButton, {marginBottom: "3.125rem"}]}
+                    {/*Create Event Button*/}
+                    <TouchableOpacity style={[GlobalStyles.submitButton, {marginBottom: "3.125rem"}]}
                             onPress={() => {
                                 navigation.navigate("Create");
                             }}
-                        >
+                            >
                             <Text style={GlobalStyles.buttonText}>
                                 Create Event
                             </Text>
                         </TouchableOpacity>
-    </View>
-
-            
-</View>
-            
+                    </View>     
+           
+            </View>
             <Footer ></Footer>
         </View>
     );
